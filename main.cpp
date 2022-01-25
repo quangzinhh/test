f60cffe07fb20c4fc0a4e149979c4dbf363de922
Quang Vinh <vinh.dangquang03@hcmut.edu.vn>
2022-01-25 20:56:19 +0700

    comitt

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=2;
+    while (a<10)
+    {
+        cout << "Gia tri cua a la" << a << endl;
+        a=a+1;
+    }
+
+    return 0;
+}
